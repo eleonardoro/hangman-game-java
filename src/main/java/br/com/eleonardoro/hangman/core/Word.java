84a0ad78a62a7ca89c30f6c47b1dfdadcf0da9a3
Eleonardo Oliveira <eleonardo.oliveira@anbetec.com.br>
2020-09-18 15:51:36 -0300

    Word Class!

--- src/main/java/br/com/eleonardoro/hangman/core/Word.java
+++ src/main/java/br/com/eleonardoro/hangman/core/Word.java
@@ -1,5 +1,57 @@
 package br.com.eleonardoro.hangman.core;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Word {
+	
+	private static final char SECRECT_CHAR = '_';
+	
+	private String originalWord;
+	private Set<Character> foundChars = new HashSet<Character>();
+	private Set<Character> wordChars = new HashSet<>();
+
+	public Word(String originalWord) {
+		this.originalWord = originalWord.toUpperCase();
+		
+		for(char c : this.originalWord.toCharArray())
+			wordChars.add(c);
+	}
+
+	public String getOriginalWord() {return originalWord;}
+
+	public void setOriginalWord(String originalWord) {this.originalWord = originalWord;}
+	
+	public int size() {return originalWord.length();}
+	
+	public boolean hasChar(char c) {
+		c = Character.toUpperCase(c);
+		
+		if (originalWord.indexOf(c) > -1) {
+			foundChars.add(c);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		for(char c : originalWord.toCharArray()) {
+			
+			if(foundChars.contains(c)) sb.append(c);
+			else sb.append(SECRECT_CHAR); 
+			
+			sb.append(" ");
+		}
+		
+		return sb.toString();
+	}
+	
+	public boolean discovered() {
+		return foundChars.equals(wordChars);
+	}
 
 }
--- src/main/java/br/com/eleonardoro/hangman/game/Game.java
+++ src/main/java/br/com/eleonardoro/hangman/game/Game.java
@@ -1,9 +1,19 @@
 package br.com.eleonardoro.hangman.game;
 
+import br.com.eleonardoro.hangman.core.Word;
+
 public class Game {
 
 	public void start() {
-		// TODO Auto-generated method stub
+		
+		Word word = new Word("casa");
+		word.hasChar('a');
+		word.hasChar('c');
+		System.out.println(word.discovered());
+		word.hasChar('s');
+		System.out.println(word.discovered());
+		
+		System.out.println(word);
 		
 	}
 

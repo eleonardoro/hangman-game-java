6cae8334560d1db30fe7a80df41ec6a641b9029a
Eleonardo Oliveira <eleonardo.oliveira@anbetec.com.br>
2020-09-22 21:06:03 -0300

    Implementing the game loop!

--- src/main/java/br/com/eleonardoro/hangman/Main.java
+++ src/main/java/br/com/eleonardoro/hangman/Main.java
@@ -1,10 +1,11 @@
 package br.com.eleonardoro.hangman;
 
+import br.com.eleonardoro.hangman.core.InvalidCharacterException;
 import br.com.eleonardoro.hangman.game.Game;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidCharacterException {
 
 		Game game = new Game();
 		game.start();
--- src/main/java/br/com/eleonardoro/hangman/game/Game.java
+++ src/main/java/br/com/eleonardoro/hangman/game/Game.java
@@ -1,17 +1,70 @@
 package br.com.eleonardoro.hangman.game;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import br.com.eleonardoro.hangman.core.Dictionary;
+import br.com.eleonardoro.hangman.core.InvalidCharacterException;
 import br.com.eleonardoro.hangman.core.Word;
+import br.com.eleonardoro.hangman.ui.UI;
 
 public class Game {
 
-	public void start() {
-		
-		Dictionary d = Dictionary.getInstance();
-				
-		
-		System.out.println(d.nextWord().getOriginalWord());
-		
+	private static final int MAX_ERRORS = 5;
+
+	public void start() throws InvalidCharacterException {
+
+		UI.print("Welcome to the Hangman Game!");
+
+		Dictionary dictionary = Dictionary.getInstance();
+		Word word = dictionary.nextWord();
+
+		UI.print("The word has " + word.size() + " letters");
+
+		char c;
+		Set<Character> usedChars = new HashSet<>();
+		int errorCount = 0;
+
+		while (true) {
+			UI.print(word);
+			UI.printNewLine();
+
+			try {
+				c = UI.readChar("Type a Letter: ");
+
+				if (usedChars.contains(c))
+					throw new InvalidCharacterException("This letter has already been typed!");
+
+				usedChars.add(c);
+
+				if (word.hasChar(c))
+					UI.print("You hit a letter!");
+				else {
+					errorCount += 1;
+
+					if (errorCount < MAX_ERRORS)
+						UI.print("You missed! You can still make " + (MAX_ERRORS - errorCount) + " mistake(s)");
+				}
+			} catch (InvalidCharacterException e) {
+				UI.print("Error: " + e.getMessage());
+				UI.printNewLine();
+			}
+
+			UI.printNewLine();
+
+			if (word.discovered()) {
+				UI.print("Congratulations! You hit the full word: " + word.getOriginalWord());
+				UI.print("End of the game!");
+				break;
+			}
+
+			if (errorCount == MAX_ERRORS) {
+				UI.print("You lost the game! The correct word was: " + word.getOriginalWord());
+				UI.print("End of the game!");
+				break;
+			}
+		}
+
 	}
 
 }
